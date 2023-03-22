bdbe8d0a4a60daa9e7c2a9d8a560857da4b01e6a
eotjd8721 <eotjd8721@github.com>
2023-03-22 22:07:25 +0900

    [CHORE] 정규표현식 수정

--- TPOMusic/Global/Extension/String+Extension.swift
+++ TPOMusic/Global/Extension/String+Extension.swift
@@ -11,15 +11,17 @@ extension String {
     
     // FIXME: - 수정 필요
     var musicTitles: [String] {
-        let pattern = "\"([^\"]*)\"|(^[^\"]*$)"
+//        let pattern = "\"([^\"]*)\"|(^[^\"]*$)"
+//        let pattern = "([\"ㄱ-ㅎ가-힣a-zA-Z]+.[\"-a-z]?.+[ㄱ-ㅎ가-힣a-zA-Z])"
+        let pattern = "(?<=\\.).+?(?=(-|by))"
         let regex = try! NSRegularExpression(pattern: pattern)
         let range = NSRange(location: 0, length: self.utf16.count)
         let matches = regex.matches(in: self, options: [], range: range)
         let matchedStrings = matches.map {
-            if $0.range(at: 1).location != NSNotFound {
-                return (self as NSString).substring(with: $0.range(at: 1))
+            if $0.range(at: 0).location != NSNotFound {
+                return (self as NSString).substring(with: $0.range(at: 0)).trimmingCharacters(in: .whitespaces)
             } else {
-                return (self as NSString).substring(with: $0.range(at: 2))
+                return (self as NSString).substring(with: $0.range(at: 1))
             }
         }
         return matchedStrings
